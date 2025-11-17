1158d8664089560893d67e1fb677154f3d1b0470
Bedir Tuğra Karaabalı <bedirkaraabali@gmail.com>
2025-11-17 11:06:05 +0300

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-// DEĞİŞİKLİK BURADA: Yolun başına "app/" eklendi
-import { AuthProvider } from "@/app/context/authContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,10 +18,7 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
         <body className={inter.className} suppressHydrationWarning>
-        {/* Tüm uygulamayı AuthProvider ile sarmala */}
-        <AuthProvider>
             {children}
-        </AuthProvider>
         </body>
         </html>
     );
